81af60103a65f4e28bfe08b6dafd6b6d0beffd52
Dawit-Engdawork <dawitengdawork33@gmail.com>
2024-10-25 03:26:06 +0300

    second commit

---
+++ resources/views/Jobs.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>Job Listing</x-slot:heading>
+    @foreach($jobs as $job)
+        <li><strong>{{$job['title']}}:</strong> pays {{$job['salary']}} per year.</li>
+    @endforeach
+</x-layout>
