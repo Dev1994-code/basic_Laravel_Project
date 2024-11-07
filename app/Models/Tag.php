5872b5e71ada397390595f2877daa6b23dc32148
Dawit-Engdawork <dawitengdawork33@gmail.com>
2024-11-07 14:47:25 +0300

    seeder and pagination

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,4 +9,12 @@ class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+
+    public function jobs() {
+        return $this->belongsToMany(Job::class, relatedPivotKey: 'job_listing_id');
+    }
+
+    public function posts() {
+        return $this->belongsToMany(Post::class);
+    }
 }
--- resources/views/Jobs.blade.php
+++ resources/views/Jobs.blade.php
@@ -1,13 +1,17 @@
 <x-layout>
     <x-slot:heading>Job Listing</x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <li>
-                <a href="jobs/{{$job['id']}}" class="text-blue-700 hover:underline">
-                    <strong>{{$job['title']}}:</strong> pays {{$job['salary']}} per year.
+                <a href="jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-400 rounded-lg">
+                    <div class="font-bold text-blue-500 text-sm">
+                        {{$job->employer->name}}
+                    </div>
+                    <div>
+                        <strong>{{$job['title']}}:</strong> pays {{$job['salary']}} per year.
+                    </div>
                 </a>
-            </li>
         @endforeach
-    </ul>
+    </div>
+    <div class='mt-8'>{{$jobs->links()}}</div>
 
 </x-layout>
